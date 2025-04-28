52374c2eba75beacdb60252774ec83394ac0149a
Janvi-Ashara <janviashara16@gmail.com>
2025-04-28 12:13:48 +0530

    Updated code

--- Server/index.js
+++ Server/index.js
@@ -19,21 +19,21 @@ mongoose
   .catch((err) => console.error("MongoDB connection error", err));
 
 // Middleware
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
-  })
-);
-
 // app.use(
 //   cors({
-//     origin: ["https://resume-builder-frontend-murex.vercel.app"],
-//     methods: ["GET","POST","PUT","DELETE"],
+//     origin: "http://localhost:3000",
+//     methods: "GET,POST,PUT,DELETE",
 //     credentials: true,
 //   })
 // );
+
+app.use(
+  cors({
+    origin: ["https://resume-builder-frontend-murex.vercel.app"],
+    methods: ["GET","POST","PUT","DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
